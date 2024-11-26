1d762627014ea15472e88cb702f40c1fd8149063
Dakshesh <dpasala@calpoly.edu>
2024-11-25 19:25:35 -0800

    Changed LOADCSV

--- utils/loadCsv.js
+++ utils/loadCsv.js
@@ -1,10 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL, // Your Supabase URL from environment variables
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // Your Supabase anon key from environment variables
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("Supabase environment variables are missing.");
+  throw new Error("Supabase environment variables are missing.");
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const loadCsvData = async () => {
   try {
