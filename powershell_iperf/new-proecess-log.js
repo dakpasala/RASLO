2277c7f460a2920ff9b15dfc6cb47e92e143546b
RandomCyberCoder <emmanueldelgado74@gmail.com>
2025-03-04 22:16:59 -0800

    added code to conver iperf's gmt time reporting to pst

--- powershell_iperf/new-proecess-log.js
+++ powershell_iperf/new-proecess-log.js
@@ -9,7 +9,16 @@ location = location.toLowercase()
         .split(' ')
         .map((str) => str.charAt(0).toUpperCase() + str.substring(1))
         .join(' ')
+/*code to read the time and format to pst */
 timeStamp = jsonBody["start"]["timestamp"]["time"]
+
+// Create a Date object from the time string
+const date = new Date(timeStamp);
+
+// Convert the date to Pacific Standard Time (PST) and output the result
+const pstDateString = date.toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
+
+console.log(pstDateString);
 //i guess we should probably check if these values are undefined
 uploadMbps = uploadBps / 1000000
 uploadMbytes_sec = uploadMbps / 8
